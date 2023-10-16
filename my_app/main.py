31daef3f1b63af25f22ad042051669e6b55e76a7
walid khirdine <khirdine.walid@gmail.com>
2023-10-16 13:59:21 +0200

    test db

--- my_app/main.py
+++ my_app/main.py
@@ -2,168 +2,244 @@ from streamlit_option_menu import option_menu
 import streamlit as st
 import sqlite3
 from passlib.hash import pbkdf2_sha256
-from utils import add_user, create_user_profile, user_profiles, create_database
+import pickle 
+import os
+from utils import *
 
-# Create or connect to the SQLite database
-conn = sqlite3.connect('recruitment.db')
-cursor = conn.cursor()
 
-# Define the image path for your logo
-logo_path = "logo/logo.png"  # Replace with the actual path to your logo
+class RecruitmentApp:
+    def __init__(self):
+        # Create or connect to the SQLite database
+        self.conn = sqlite3.connect('recruitment.db')
+        self.cursor = self.conn.cursor()
+        self.logo_path = "logo/logo.png"
+        self.load_session_state()
+        self.initialize_session_state()
 
-# Initialize the user session state
-if 'user' not in st.session_state:
-    st.session_state.user = None
+    def initialize_session_state(self):
+        # Try to load the session state
+        self.load_session_state()
+        # If it's not loaded, initialize with default values
+        if 'user' not in self.session_state:
+            self.session_state['user'] = None
+            
 
-# Define a function to show the sidebar and return the selected option
-def show_sidebar():
-    st.sidebar.image(logo_path, width=300)
-    with st.sidebar:
-        sidebar_options = ["Home", "Search Jobs", "Login"]
-        if st.session_state.user:
-            sidebar_options.append("Change Password")
-            sidebar_options.remove("Login")
-        else:
-            sidebar_options.append("Register")
-        sidebar_options.append("User Profile")
-        selected = option_menu("Main Menu", sidebar_options, icons=['house', 'lightbulb', 'star', 'person', 'gear'], menu_icon="cast", default_index=1)
-    return selected
+    def load_session_state(self):
+        try:
+            with open('session_state.pkl', 'rb') as file:
+                self.session_state = pickle.load(file)
+        except FileNotFoundError:
+            self.session_state = {'user': None}
 
-# Define a function to show the login form and authenticate the user
-def login_user():
-    st.title("User Login")
-    email = st.text_input('Email')
-    password = st.text_input('Password', type='password')
-    if st.button('Login', key="login_button"):
-        user = authenticate_user(email, password)
-        if user:
-            st.success('Login successful!')
-            st.empty()
-            st.session_state.user = user[4]  # Store the username in the session
-            st.write(f'Hello, {user[4]}!')
-            st.write('You are a recruiter.' if user[2] else 'You are a candidate.')
-        else:
-            st.error('Login failed. Check your credentials.')
-
-def update_password(user_id, new_hashed_password, conn):
-    try:
-        cursor = conn.cursor()
-        cursor.execute('UPDATE users SET password = ? WHERE id = ?', (new_hashed_password, user_id))
-        conn.commit()
-    except sqlite3.Error as e:
-        st.error(f"Error updating password: {e}")
-
-# Define a function to authenticate the user
-def authenticate_user(email, password):
-    cursor.execute(
-        'SELECT id, password, is_recruiter, email_address, username FROM users WHERE email_address = ?', (email,))
-    user = cursor.fetchone()
-    return user if user and pbkdf2_sha256.verify(password, user[1]) else None
-
-# Define a function to register a new user
-def register_user():
-    st.title("User Registration")
-    new_username = st.text_input('Username')
-    new_password = st.text_input('Password', type='password')
-    new_email = st.text_input('Email')
-    is_recruiter = st.checkbox('I am a recruiter')
-
-    if st.button('Register', key="register_button"):
-        add_user(new_username, new_password, new_email, is_recruiter, conn)
-        st.success('Registration successful! You can now log in.')
-
-# Define a function to show the change password form
-def show_change_password():
-    st.title("Change Password")
-    if st.session_state.user:
-        username = st.session_state.user
-        # Create input fields for the current password and the new password
-        current_password = pbkdf2_sha256.hash(st.text_input('Current Password', type='password'))
-        new_password = st.text_input('New Password', type='password')
-        confirm_password = st.text_input('Confirm New Password', type='password')
-        if st.button("Change Password"):
-            if not new_password:
-                st.error("New password cannot be empty.")
-            elif new_password != confirm_password:
-                st.error("New passwords do not match.")
-            else:
-                user_data = fetch_user_data(username)
-                if user_data:
-                    user_id, current_hashed_password = user_data[0], user_data[1]
-                    if pbkdf2_sha256.verify(current_password, current_hashed_password):
-                        # Passwords match, update the password
-                        new_hashed_password = pbkdf2_sha256.hash(new_password)
-                        update_password(user_id, new_hashed_password)
-                        st.success("Password changed successfully.")
-                    else:
-                        st.error("Current password is incorrect.")
+    def save_session_state(self):
+        with open('session_state.pkl', 'wb') as file:
+            pickle.dump(self.session_state, file)
+            
+    def log_out(self):
+        self.session_state['user'] = None
+        
+    def show_sidebar(self):
+        st.sidebar.image(self.logo_path, width=300)
+        with st.sidebar:
+            sidebar_options = ["Home", "Search Jobs", "Login"]
+            if not self.login_user()[1]:
+                if self.session_state['user']:
+                    sidebar_options.append("Change Password")
+                    sidebar_options.remove("Login")
+                    sidebar_options.append("Log Out")
                 else:
-                    st.error("User not found. Please log in.")
+                    sidebar_options.append("Register")
+                sidebar_options.append("User Profile")
+                selected = option_menu("Main Menu", sidebar_options, icons=['house', 'lightbulb', 'star', 'person', 'gear'], menu_icon="cast", default_index=1)
+            else:
+                if self.session_state['user']:
+                    sidebar_options.append("Change Password")
+                    sidebar_options.remove("Login")
+                    sidebar_options.append("Log Out")
+                else:
+                    sidebar_options.append("Register")
+                sidebar_options.append("User Profile")
+                selected = option_menu("Main Menu", sidebar_options, icons=['house', 'lightbulb', 'star', 'person', 'gear'], menu_icon="cast", default_index=1)
+        return selected
+    
+    def authenticate_user(self, email, password):
+        self.cursor.execute(
+            'SELECT id, password, is_recruiter, email_address, username FROM users WHERE email_address = ?', (email,))
+        user = self.cursor.fetchone()
+        return user if user and pbkdf2_sha256.verify(password, user[1]) else None
+    
+    def login_user(self):
+        st.title("User Login")
+        email = st.text_input('Email')
+        password = st.text_input('Password', type='password')
+        if st.button('Login', key="login_button"):
+            user = self.authenticate_user(email, password)
+            if user:
+                st.success('Login successful!')
+                st.empty()
+                self.session_state['user'] = user[4]  # Store the username in the session
+                self.save_session_state()
+                st.write(f'Hello, {user[4]}!')
+                if user[2]:  # Check if the user is a recruiter
+                    st.write('You are a recruiter.')
+                    return 0,user[0]
+                else:
+                    st.write('You are a candidate.')
+                    return 1,user[0]
+            else:
+                return st.error('Login failed. Check your credentials.')
+                
+
+    def update_password(self, user_id, new_hashed_password):
+        try:
+            self.cursor.execute('UPDATE users SET password = ? WHERE id = ?', (new_hashed_password, user_id))
+            self.conn.commit()
+            st.success('Password changed successfully!')
+            # Update session state to avoid log out on page refresh
+            self.session_state['user'] = self.fetch_user_data(self.session_state['user'])[3]
+            self.save_session_state()
+        except sqlite3.Error as e:
+            st.error(f"Error updating password: {e}")
+
+
+    def register_user(self):
+        st.title("User Registration")
+        new_username = st.text_input('Username')
+        new_password = st.text_input('Password', type='password')
+        new_email = st.text_input('Email')
+        is_recruiter = st.checkbox('I am a recruiter')
+        if st.button('Register', key="register_button"):
+            self.add_user(new_username, new_password, new_email, is_recruiter)
+            st.success('Registration successful! You can now log in.')
+            self.session_state['user'] = new_username
+            self.save_session_state()
+
+    def add_user(self, username, password, email, is_recruiter):
+        hashed_password = pbkdf2_sha256.hash(password)
+        self.cursor.execute('INSERT INTO users (username, password, email_address, is_recruiter) VALUES (?, ?, ?, ?)',
+                            (username, hashed_password, email, is_recruiter))
+        self.conn.commit()
+
+    def update_user_data(self, username, email, profile_picture, cv):
+        # self.cursor.execute('UPDATE users SET email_address = ? WHERE username = ?', (email, username))
+        # self.conn.commit()
+        if profile_picture:
+            profile_picture_path = os.path.join(f"data/user_{username}/profile_picture/{username}.jpg")
+            os.makedirs(os.path.dirname(profile_picture_path),exist_ok=True)
+            with open(profile_picture_path, "wb") as f:
+                f.write(profile_picture.read())
+            save_profile_picture(profile_picture_path, username)
+        if cv:
+            cv_path = os.path.join(f"data/user_{username}/CV/{username}_cv.pdf")
+            os.makedirs(os.path.dirname(cv_path),exist_ok=True)
+            with open(cv_path, "wb") as f:
+                f.write(cv.read())
+            save_cv_path(cv_path,username)
+                
+    def update_recruiter_data(self, name_offer, username, profile_picture, job_offer):
+        if profile_picture:
+            profile_picture_path = os.path.join(f"data/user_{username}/profile_picture/{username}.jpg")
+            os.makedirs(os.path.dirname(profile_picture_path),exist_ok=True)
+            with open(profile_picture_path, "wb") as f:
+                f.write(profile_picture.read())
+            save_profile_picture(profile_picture_path,username)
+        elif job_offer:
+            job_offer_path = os.path.join(f"data/user_{username}/offres/{name_offer}.pdf")
+            os.makedirs(os.path.dirname(job_offer_path),exist_ok=True)
+            with open(job_offer_path, "wb") as f:
+                f.write(job_offer.read())
+            save_job_offer(self.login_user()[1],job_offer_path,name_offer)
+            
+    def show_user_profile(self):
+        st.title("User Profile")
+        if self.session_state['user']:
+            username = self.session_state['user']
+            user_data = self.fetch_user_data(username)
+            if user_data[4]:
+                username, email = user_data[1], user_data[3]
+                st.write(f"Username: {username}")
+                st.write(f"Email: {email}")
+                st.header("Edit Profile")
+                uploaded_profile_picture = st.file_uploader("Upload Profile Picture", type=["jpg", "jpeg", "png"])
+                uploaded_cv = st.file_uploader("Upload CV (PDF)", type=["pdf"])
+                if st.button("Save Changes"):
+                    self.update_user_data(username, email, uploaded_profile_picture, uploaded_cv)
+                st.success("Changes saved successfully!")
+            else:
+                username, email = user_data[1], user_data[3]
+                st.write(f"Username: {username}")
+                st.write(f"Email: {email}")
+                st.header("Edit Profile")
+                uploaded_profile_picture = st.file_uploader("Upload Profile Picture", type=["jpg", "jpeg", "png"])
+                uploaded_offer = st.file_uploader("Upload offer (PDF)", type=["pdf"])
+                if st.button("Save Changes"):
+                    self.update_recruiter_data(username, email, uploaded_profile_picture, uploaded_offer)
+                st.success("Changes saved successfully!")
+                    
+                st.error("User not found. Please log in.")
         else:
-            st.warning("Fill in the password fields and click 'Change Password' to proceed.")
-    else:
-        st.error("You are not logged in. Please log in to change your password.")
+            st.error("You are not logged in. Please log in to view your profile.")
 
 
-# Define a function to show the user profile
-def show_user_profile():
-    st.title("User Profile")
-    if st.session_state.user:
-        username = st.session_state.user
-        user_data = fetch_user_data(username)
+    def fetch_user_data(self, username):
+        self.cursor.execute('SELECT id, username, password, email_address, is_recruiter FROM users WHERE username = ?', (username,))
+        return self.cursor.fetchone()
 
-        if user_data:
-            username, email = user_data[0], user_data[1]
-
-            st.write(f"Username: {username}")
-            st.write(f"Email: {email}")
-
-            st.header("Edit Profile")
-            uploaded_profile_picture = st.file_uploader("Upload Profile Picture", type=["jpg", "jpeg", "png"])
-            uploaded_cv = st.file_uploader("Upload CV (PDF)", type=["pdf"])
-
-            if st.button("Save Changes"):
-                update_user_data(username, email, uploaded_profile_picture, uploaded_cv)
-
-            st.success("Changes saved successfully!")
+    def show_change_password(self):
+        st.title("Change Password")
+        if self.session_state['user']:
+            username = self.session_state['user']
+            current_password = pbkdf2_sha256.hash(st.text_input('Current Password', type='password'))
+            new_password = st.text_input('New Password', type='password')
+            confirm_password = st.text_input('Confirm New Password', type='password')
+            if st.button("Change Password"):
+                if not new_password:
+                    st.error("New password cannot be empty.")
+                elif new_password != confirm_password:
+                    st.error("New passwords do not match.")
+                else:
+                    user_data = self.fetch_user_data(username)
+                    if user_data:
+                        user_id, current_hashed_password = user_data[0], user_data[2]
+                        if pbkdf2_sha256.verify(current_password, current_hashed_password):
+                            new_hashed_password = pbkdf2_sha256.hash(new_password)
+                            self.update_password(user_id, new_hashed_password)
+                            st.success("Password changed successfully.")
+                        else:
+                            st.error("Current password is incorrect.")
+                    else:
+                        st.error("User not found. Please log in.")
+            else:
+                st.warning("Fill in the password fields and click 'Change Password' to proceed.")
         else:
-            st.error("User not found. Please log in.")
-    else:
-        st.error("You are not logged in. Please log in to view your profile.")
+            st.error("You are not logged in. Please log in to change your password.")
 
-# Define a function to fetch user data from the database
-def fetch_user_data(username):
-    cursor.execute('SELECT username, email_address FROM users WHERE username = ?', (username,))
-    return cursor.fetchone()
 
-# Define a function to update user data in the database
-def update_user_data(username, email, profile_picture, cv):
-    # Add your logic to update user data and handle file uploads
-    pass
 
-# Main app logic
-selected_option = show_sidebar()
+if __name__ == "__main__":
+    app = RecruitmentApp()
+    create_database(app.conn,app.cursor)
+    selected_option = app.show_sidebar()
+    if selected_option == "Home":
+        st.title("Welcome to Your Recruitment Platform")
+        st.write("Browse the latest job listings below:")
+        # Display job listings here
 
-if selected_option == "Home":
-    st.title("Welcome to Your Recruitment Platform")
-    st.write("Browse the latest job listings below:")
-    # Display job listings here
+    elif selected_option == "Search Jobs":
+        st.title("Search for Jobs")
+        # Implement your job search functionality
 
-elif selected_option == "Search Jobs":
-    st.title("Search for Jobs")
-    # Implement your job search functionality
+    elif selected_option == "Login":
+        app.login_user()
 
-elif selected_option == "Login":
-    login_user()
+    elif selected_option == "Register":
+        app.register_user()
 
-# Main app logic (continued)
+    elif selected_option == "Change Password":
+        app.show_change_password()
 
-elif selected_option == "Register":
-    register_user()
-
-elif selected_option == "Change Password":
-    show_change_password()
-
-elif selected_option == "User Profile":
-    show_user_profile()
+    elif selected_option == "User Profile":
+        app.show_user_profile()
+    elif selected_option == "Log Out":
+        app.log_out()
 
--- my_app/utils.py
+++ my_app/utils.py
@@ -6,30 +6,82 @@ from datetime import datetime
 def create_database(conn, cursor):
     conn = sqlite3.connect('recruitment.db')
     cursor = conn.cursor()
-
     # Create the users table
+
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            username TEXT NOT NULL,
+        CREATE TABLE IF NOT EXISTS Users (
+            user_id INTEGER PRIMARY KEY,
+            username TEXT UNIQUE NOT NULL,
             password TEXT NOT NULL,
             email_address TEXT NOT NULL,
-            is_recruiter INTEGER NOT NULL
+            is_recruiter BOOLEAN  NOT NULL,
+            profile_picture text,
+            cv_path text
         )
     ''')
 
-    # Create other tables (e.g., job listings and applications) if needed
+    # Create Job Offers Table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Job_offers (
+            job_id INTEGER PRIMARY KEY,
+            recruiter_id INTEGER NOT NULL,
+            offer_path TEXT NOT NULL,
+            name_offer TEXT NOT NULL
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            location TEXT NOT NULL,
+            salary REAL,
+            FOREIGN KEY (recruiter_id) REFERENCES Users (user_id)
+        )
+    ''')
 
+    # Create Applications Table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Applications (
+            application_id INTEGER PRIMARY KEY,
+            job_id INTEGER NOT NULL,
+            candidate_id INTEGER NOT NULL,
+            application_status TEXT NOT NULL,
+            -- Add other application-specific fields here
+            FOREIGN KEY (job_id) REFERENCES JobOffers (job_id),
+            FOREIGN KEY (candidate_id) REFERENCES Users (user_id)
+        )
+    ''')
+    # Create other tables (e.g., job listings and applications) if needed
     conn.commit()
     conn.close()
 
 # Helper function to add a user
+
+
 def add_user(username, email_address, password, is_recruiter, conn, cursor):
     password_hash = pbkdf2_sha256.hash(password)
-    cursor.execute('INSERT INTO users (username, password, email_address, is_recruiter) VALUES (?, ?, ?, ?)', (username, password_hash, email_address, is_recruiter))
+    cursor.execute('INSERT INTO users (username, password, email_address, is_recruiter) VALUES (?, ?, ?, ?)',
+                   (username, password_hash, email_address, is_recruiter))
     conn.commit()
 
+
+def save_job_offer(recruiter_id, job_offer_path, name_offer, conn, cursor):
+    cursor.execute('INSERT INTO job_offers (recruiter_id,offer_path,name_offer) VALUES (?, ?, ?, ?)',
+                   (recruiter_id, job_offer_path, name_offer))
+    conn.commit()
+
+
+def save_profile_picture(profile_picture_path, username, conn, cursor):
+    cursor.execute('UPDATE users SET profile_picture = ? WHERE username = ?',
+                   (profile_picture_path, username))
+    conn.commit()
+
+
+def save_cv_path(cv_path, username, conn, cursor):
+    cursor.execute(
+        'UPDATE users SET cv_path = ? WHERE username = ?', (cv_path, username))
+    conn.commit()
+
+
 user_profiles = []  # Initialize the user profiles list
+
+
 def create_user_profile(user_profiles, new_name, new_email, new_id, profile_picture, cv_file, conn, cursor):
     current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     new_profile = {
